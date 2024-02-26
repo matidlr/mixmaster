56bd907d9732a2b1ce67a11a306f0ad5a591b651
Matias <mati.delarosa1984@gmail.com>
2024-02-26 17:00:51 -0300

    cocktail card and list

--- src/pages/Cocktail.jsx
+++ src/pages/Cocktail.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 
-export default function Cocktail() {
+const Cocktail = () => {
   return (
     <div>Cocktail</div>
   )
 }
+
+export default Cocktail
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import { useLoaderData } from 'react-router-dom';
+import axios from 'axios';
+import CocktailList from '../components/CocktailList';
+const cocktailSearchUrl = 'www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita';
+
+export const loader = async () => {
+  const searchTerm = 'margarita';
+  const response = await axios.get(`${cocktailSearchUrl}${searchTerm}`);
+  console.log(response)
+  return { drinks: response.data.drinks, searchTerm};
+
+}
 
 export default function Landing() {
+   const {drinks, searchTerm } = useLoaderData();
   return (
-    <div>Landing</div>
+    <>
+      <CocktailList/>
+    </>
   )
 }
