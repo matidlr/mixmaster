ccebd84c65c2290e624f37d5fdde15cfe8b963b7
Matias <mati.delarosa1984@gmail.com>
2024-02-27 14:14:34 -0300

    styled

--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,5 +1,5 @@
 import { Outlet, useNavigation } from 'react-router-dom';
-import Navbar from '../components/Navbar';
+import Navbar from '../components/NavBar';
 
 const HomeLayout = () => {
   const navigation = useNavigation();
